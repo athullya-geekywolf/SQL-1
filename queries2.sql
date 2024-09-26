a0ff9d067b2dd3b70a52a753fe514b38a6c898ea
athullya-geekywolf <athullya.r@geekywolf.com>
2024-09-26 18:12:33 +0530

    Update queries2.sql

--- queries2.sql
+++ queries2.sql
@@ -11,4 +11,31 @@ SELECT * FROM EMPLOYEES
 WHERE DEPARTMENT IN ('SALES','MARKETING','IT');
 
 --Find all employees with salaries ranging from $50,000 to $75,000 (inclusive).
+SELECT * FROM EMPLOYEES WHERE SALARY BETWEEN 50000 AND 75000;
+
+--List all employees whose last name begins with the letter 'S'.
+SELECT * FROM EMPLOYEES 
+WHERE LASTNAME 
+LIKE 'S%';
+
+--Display all employees with exactly five letters in their first name
+SELECT * FROM EMPLOYEES 
+WHERE FIRSTNAME 
+LIKE '_____';
+
+--Find employees whose last name starts with either 'B', 'R', or 'S'.
+SELECT *
+FROM Employees
+WHERE LastName LIKE 'B%'
+   OR LastName LIKE 'R%'
+   OR LastName LIKE 'S%';
+
+--Retrieve all employees whose first name begins with any letter from 'A' through 'M'.
+SELECT *
+FROM Employees
+WHERE FirstName LIKE '[A-M]%';
+
+
+
+
 
