6adce908c6decdc77f554b645d40070ddad8daaa
athullya-geekywolf <athullya.r@geekywolf.com>
2024-09-26 11:13:19 +0530

    Print all prime numbers below 100

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿public class AllPrime
+{
+    public static void Main(string[] args)
+    {
+        int number=100;
+        
+        
+        Console.WriteLine("Prime numbers under 100");
+        for(int i=2;i<number; i++) 
+        {
+            int flag = 0;
+            for (int j = 2; j <= i / 2; j++)
+            {
+                if (i % j == 0)
+                {
+                    flag = 1;
+                    break;
+                   
+                    
+                }
+
+            }
+            if (flag == 0)
+            {
+                Console.WriteLine(i);
+            }
+
+            
+        }
+        
+    }
+}
