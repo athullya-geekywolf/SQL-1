4bf5c94a6178972cff68c837a4bf402c4cae0915
athullya-geekywolf <athullya.r@geekywolf.com>
2024-09-26 17:13:57 +0530

    Create queries2.sql

---
+++ queries2.sql
@@ -0,0 +1,13 @@
+--Create a table students and insert names in malayalam
+CREATE TABLE STUDENTS(
+STUD_ID INT,
+NAME NVARCHAR(30)
+);
+
+INSERT INTO STUDENTS VALUES(02,N'റാം');
+
+--Retrieve all employees who work in Sales, Marketing, or IT departments.
+SELECT * FROM EMPLOYEE
+WHERE DEPARTMENT IN ('SALES','MARKETING','IT');
+
+--Find all employees with salaries ranging from $50,000 to $75,000 (inclusive).
