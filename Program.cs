169dbfe691337f34a1f3c5990104b5d584e29d12
athullya-geekywolf <athullya.r@geekywolf.com>
2024-09-26 11:18:39 +0530

    tax calculation

--- Program.cs
+++ Program.cs
@@ -1,16 +1,31 @@
-﻿public class SumOfSquares
+﻿public class Tax
 {
-    public static void Main(string[] args)
+    public static void Main(String[] args)
     {
-        Console.WriteLine("enter number");
-        int number=Convert.ToInt32(Console.ReadLine());
-        int sum_of_squares = 0;
-        while (number > 0)
+        Console.WriteLine("enter the amount");
+        double tax;
+
+
+        int amount = Convert.ToInt32(Console.ReadLine());
+        if (amount < 10000)
         {
-            int last = number % 10;
-            sum_of_squares=sum_of_squares+last*last;
-            number = number / 10;
+            Console.WriteLine((5.0 / 100) * amount);
         }
-        Console.WriteLine(sum_of_squares);
+        else if (amount > 10000 & amount < 15000)
+        {
+            Console.WriteLine((7.5 / 100) * amount);
+        }
+        else if (amount > 15000 & amount < 20000)
+        {
+            Console.WriteLine((10.0 / 100) * amount);
+        }
+        else if (amount > 20000 & amount < 25000)
+        {
+            Console.WriteLine((12.5 / 100) * amount);
+        }
+        else
+        {
+            Console.WriteLine((15.0 / 100) * amount);
+        }
     }
 }
