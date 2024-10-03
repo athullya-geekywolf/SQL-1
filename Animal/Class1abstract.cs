17b0dc730fa6a3d4390e434e03fd1e28367b2356
Athullya R <athullya.r@geekywolf.local>
2024-10-03 15:27:51 +0530

    Animals project added

---
+++ Animal/Class1abstract.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Animals
+{
+    public abstract class Animal
+    {
+        public abstract string Name { get; set; }
+        public abstract string Sound { get; set; }
+
+        public abstract void MakeSound();
+    }
+}
